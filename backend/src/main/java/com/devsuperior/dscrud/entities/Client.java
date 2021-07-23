1884e3cdde1bab56f3847ea1d8946aff8eda3932
Wellington Pessanha <ricardo.wellington@hotmail.com>
2021-07-23 02:17:15 -0300

    SQL script, finish dscrud

--- backend/src/main/java/com/devsuperior/dscrud/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dscrud/entities/Client.java
@@ -3,13 +3,10 @@ package com.devsuperior.dscrud.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -26,11 +23,6 @@ public class Client implements Serializable {
 	private Instant birthDate;
 	private Integer children;
 	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant createdAt;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant updatedAt;
 	
 	public Client() {
 	}
@@ -96,25 +88,7 @@ public class Client implements Serializable {
 	public Instant getBirthDate() {
 		return birthDate;
 	}
-	
 
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
-
-	
-	public Instant getUpdatedAt() {
-		return updatedAt;
-	}
-	@PrePersist
-	public void prePersist() {
-		createdAt = Instant.now();
-	}
-	@PreUpdate
-	public void preUpdate() {
-		updatedAt = Instant.now();
-	}
-	
 
 	@Override
 	public int hashCode() {
